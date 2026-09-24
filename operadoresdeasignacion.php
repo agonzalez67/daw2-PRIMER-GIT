db1e68c489bc4f83362ac24a4fc1b958b5d0e8d1
agonzalez67 <agonzaleza@academiacrespo.com>
2026-09-24 17:40:38 +0200

    Angel es una caca

---
+++ operadoresdeasignacion.php
@@ -0,0 +1,32 @@
+<?php
+
+$x = 10;
+
+
+
+echo "<h1>Valor inicial de x: $x</h1>";
+
+
+$x = $x + 5;
+echo "Después de sumar 5: $x<br>";
+
+
+$x = $x - 3;
+echo "Después de restar 3: $x<br>";
+
+
+$x = $x * 2;
+echo "Después de multiplicar por 2: $x<br>";
+
+
+$x = $x / 4;
+echo "Después de dividir entre 4: $x<br>";
+
+
+$x++;
+echo "Después de incrementar en 1: $x<br>";
+
+
+$x -= 2;
+echo "Después de decrementar en 2: $x<br>";
+?>
